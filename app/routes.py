b6252a68f381ec6797e8c0883e49f4f1c9f1bb3c
Jeremie Tamburini <jeremie.tamburini@gmail.com>
2020-01-29 18:08:59 +0100

    Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import render_template, url_for
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return 'ciao'
+    return render_template('home.html', title='Home')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
