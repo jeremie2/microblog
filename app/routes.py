2a27cdfd96eaff93f337b31710864919607c3ce4
Jeremie Tamburini <jeremie.tamburini@gmail.com>
2020-01-29 21:27:52 +0100

    Html Login

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
 from app import app
 from flask import render_template, url_for
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template('home.html', title='Home')
+    user = {'username': 'Miguel'}
+    return render_template('home.html', title='Home', user=user)
 
 @app.route('/login')
 def login():
-    return render_template('login.html', title='Login')
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
