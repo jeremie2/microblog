1ccde496957a78e1981d3cee499f84386faedf23
Jeremie Tamburini <jeremie.tamburini@gmail.com>
2020-01-31 01:34:44 +0100

    Html Login - improved

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,19 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
 from app.forms import LoginForm
 
 
 @app.route('/')
+@app.route('/index')
 @app.route('/home')
 def index():
     user = {'username': 'Miguel'}
     return render_template('home.html', title='Home', user=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Login', form=form)
